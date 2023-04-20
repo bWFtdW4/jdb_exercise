54f35205d7d89b396d704656bc2828c8e0c931f4
bWFtdW4 <128370816+bWFtdW4@users.noreply.github.com>
2023-04-20 13:52:49 +0200

    Create BinaryOperators.java

---
+++ src/edu/damago/jdb/tool/BinaryOperators.java
@@ -0,0 +1,22 @@
+package edu.damago.jdb.tool;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.DoubleBinaryOperator;
+
+
+/**
+ * Instances of this class represent arithmetic binary operators.
+ */
+public class BinaryOperators {
+	static public final Map<String,DoubleBinaryOperator> POOL = new HashMap<>();
+
+	static {
+		POOL.put("+", (l, r) -> l + r);
+		POOL.put("-", (l, r) -> l - r);
+		POOL.put("*", (l, r) -> l * r);
+		POOL.put("/", (l, r) -> l / r);
+		POOL.put("%", (l, r) -> l % r);
+		POOL.put("**", (l, r) -> Math.pow(l, r));
+	}
+}
