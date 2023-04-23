23e4e14f7589365a96b45ff5d5869ae4b80b9631
bWFtdW4 <128370816+bWFtdW4@users.noreply.github.com>
2023-04-23 14:18:49 +0200

    Update Statictics2App.java

--- src/edu/damago/jdb/calc/Statictics2App.java
+++ src/edu/damago/jdb/calc/Statictics2App.java
@@ -1,8 +1,6 @@
 package edu.damago.jdb.calc;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 
 /**
@@ -23,7 +21,6 @@ public class Statictics2App {
 	static public void main (final String[] args) throws IOException {
 		System.out.println("starting...");
 		final Statistics2Controller controller = new Statistics2Controller();
-		final BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 
 		controller.performWelcome();
 		
